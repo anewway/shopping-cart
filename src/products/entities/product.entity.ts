ae2d72e6527c6d02c5b36c9e38950d2603d54744
Amy Liou <sourpluslot@gmail.com>
2024-03-11 16:31:26 +0800

    Add basic database connection and product entity definitions.

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,22 @@
-export class Product {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  quantity: number;
+  
+  @Column()
+  create_time: number;
+
+  @Column()
+  update_time: number;
+}
